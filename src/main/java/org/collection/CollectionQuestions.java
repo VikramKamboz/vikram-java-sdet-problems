5b7adf55d6e5d29e76f7e13ef7aeb4c374b26d3d
VikramKumar1 <vikramkamboj14@gmail.com>
2026-06-24 18:26:24 +0530

    reverse and palindrome program

--- src/main/java/org/collection/CollectionQuestions.java
+++ src/main/java/org/collection/CollectionQuestions.java
@@ -1,17 +1,64 @@
-package org.example;
+package org.collection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class CollectionQuestions {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        findDuplicateIds();
+        reverseAndPalindrome();
+        reverseAndPalindromeTwoPointer();
+
     }
+
+    //P1: Find Duplicate Test Case IDs
+    private static void findDuplicateIds() {
+        String[] input = {"TC001", "TC002", "TC001", "TC003", "TC002", "TC004"};
+        TreeSet<String> duplicates = new TreeSet<>();
+        TreeSet<String> seen = new TreeSet<>();
+        for (String s : input) {
+            if (seen.contains(s)) {
+                duplicates.add(s);
+            } else {
+                seen.add(s);
+            }
+        }
+        System.out.println("duplicates : " + duplicates);
+        System.out.println("seen : " + seen);
+    }
+
+
+    //P2: Reverse a String and Check Palindrome
+    private static void reverseAndPalindrome() {
+        String inputString = "racecar";
+        boolean isPalindrome = false;
+        StringBuilder sb = new StringBuilder();
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            sb.append(inputString.charAt(i));
+        }
+        if (sb.toString().equalsIgnoreCase(inputString)) {
+            isPalindrome = true;
+        }
+        System.out.println("isPalindrome : " + isPalindrome);
+    }
+
+    //P2: Reverse a String and Check Palindrome
+    private static void reverseAndPalindromeTwoPointer() {
+        String inputString = "racecar";
+        int start = 0;
+        int end = inputString.length() - 1;
+        while (start < end) {
+            if (inputString.charAt(start) != inputString.charAt(end)) {
+                System.out.println("is not palindrome : " + inputString);
+                return;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("is palindrome : " + inputString);
+    }
+
+
 }
